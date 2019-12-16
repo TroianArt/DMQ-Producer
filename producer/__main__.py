81c3122e32bc5b6e2e9d2bc4ef3412d1cf57113c
shyshach <shymchak198@gmail.com>
2019-12-16 10:27:07 +0200

    Closing #3 Implement basic app logic.

---
+++ producer/__main__.py
@@ -0,0 +1,46 @@
+import requests
+
+
+class Producer:
+    def __init__(self, manager_url, queue_id, load_balancing_strategy="size"):
+        self.__manager_host, self.__manager_port = manager_url
+        self.__load_balancing_strategy = load_balancing_strategy
+        self.queue_id = queue_id
+        self.__current_node = {}
+        self.__current_request = 0
+        self.number_of_requests = 10
+
+    def get_statistics(self):
+        response = requests.get(self.__manager_host + ':' + self.__manager_port + '/statistics/')
+        return response.json()
+
+    def balance_by_cpu(self):
+        statistics = self.get_statistics()
+        current_node = statistics['data_nodes'][0]
+        for node in statistics['data_nodes']:
+            if node["cpu_load"] <= current_node["cpu_load"]:
+                current_node = node
+        self.__current_node = current_node
+        return self.__current_node
+
+    def balance_by_size(self):
+        statistics = self.get_statistics()
+        current_node = statistics['data_nodes'][0]
+        for node in statistics['data_nodes']:
+            for queue in node['queues']:
+                queue_id = queue['id']
+                current_queue = list(filter(lambda x: x['id'] == queue_id, current_node['queues']))[0]
+                if queue_id == self.queue_id and queue['size'] <= current_queue['size']:
+                    current_node = node
+        self.__current_node = current_node
+        return self.__current_node
+
+    def rebalance(self):
+        if self.__load_balancing_strategy == 'cpu':
+            self.balance_by_cpu()
+        self.balance_by_size()
+
+    def produce(self, key, value):
+        url = 'http://' + self.__manager_host + ':' + self.__manager_port
+        response = requests.post(url, json={key: value})
+        return response.json()
